07899d63e6da1a6035dd103768bfdb869f448652
Double-Blank <a13633205983@163.com>
2022-01-15 00:05:21 +0800

    一些修改

---
+++ docs/.vuepress/audios.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    name: "Los Angeles",
+    artist: "The Midnight",
+    url:
+      "http://ws.stream.qqmusic.qq.com/C400003bf37T44XXgH.m4a?guid=419820764&vkey=D1725A3FF65873DDC0D4631229305307EDB65D4BA121BEB98AA464A870426527E0C30A516F99622E6587DBC8F66B5441CC7ED124ED933B01&uin=&fromtag=66",
+    cover:
+      "http://p1.music.126.net/xw-Jg5vjV2bfWYBT-zKbVw==/109951165394997449.jpg?param=130y130",
+  },
+  {
+    name: "夏日漱石",
+    artist: "橘子海",
+    url:
+      "http://ws.stream.qqmusic.qq.com/C400002T3u8J46Hal3.m4a?guid=688310962&vkey=3CFCC2F8D752C4B6B5BB3F8F7796667E64244B3C46239CA94661897674CF595A4B707FA1AD10C52A61689B330958A23560992144ECC5C8DA&uin=&fromtag=66",
+    cover:
+      "https://y.gtimg.cn/music/photo_new/T002R300x300M000001goINS2qAQS6.jpg",
+  }
+]
